fd99fa7803dff668e34d4e2f31617e868412ae31
Дарья Грачева <daherishka@yandex.ru>
2022-09-05 20:43:56 +0500

    1st task is done

--- js-features/countdown/task.js
+++ js-features/countdown/task.js
@@ -1,4 +1,10 @@
-const timerEnd = document.getElementById("timer");
 function countdown() {
-    
+    const timer = document.getElementById("timer");
+    --timer.textContent;
+     if (timer.textContent <= 0){
+        alert("Вы победили в конкурсе");
+        clearInterval(interval);
+    }
 }
+
+const interval = setInterval(countdown, 1000);
