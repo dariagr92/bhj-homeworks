21af3ada50b4c7f719e48d25c3a7dabd97d843e9
Дарья Грачева <daherishka@yandex.ru>
2022-09-06 16:34:30 +0500

    1st to be continued

---
+++ element-search/popups/task.js
@@ -0,0 +1,6 @@
+let main = document.getElementsById('modal_main');
+let close = Array.From(document.getElementsByClassName('modal__close'));
+let success = document.getElementsById('modal_success');
+let show = Array.from(document.getElementsByClassName('show-success'));
+
+main.classList  = "modal modal_active";
