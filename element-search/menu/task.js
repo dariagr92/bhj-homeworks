38a8fe7766efa00b97a5b66ff22d74b5e8b92562
Дарья Грачева <daherishka@yandex.ru>
2022-09-07 21:49:34 +0500

    2nd to finish

---
+++ element-search/menu/task.js
@@ -0,0 +1,21 @@
+let menu = Array.from(document.querySelectorAll(".menu__link"));
+let subMenu = Array.from(document.querySelectorAll("ul.menu.menu_sub"))
+
+for (let i = 0; i < menu.length; i++){
+    menu[i].onclick = function() {
+        const choosen = menu[i].closest(".menu__item").querySelector("menu_sub");
+        const show = subMenu.find((item) => item.classList.contains("menu_active"));
+
+        if (choosen){
+            if (show === undefined){
+                choosen.classList.add('menu_active');
+            } else {
+                if (show !== choosen){
+                    choosen.classList.add('menu_active'); 
+                }
+                show.classList.remove('menu_active');
+            }
+           return false; 
+        }
+    };
+}
