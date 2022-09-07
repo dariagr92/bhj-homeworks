f232da3865b6e1ffe66544804d2dc3bab7cf26b9
Дарья Грачева <daherishka@yandex.ru>
2022-09-07 20:22:58 +0500

    1st is not wokred

--- element-search/popups/task.js
+++ element-search/popups/task.js
@@ -1,6 +1,14 @@
-let main = document.getElementsById('modal_main');
-let close = Array.From(document.getElementsByClassName('modal__close'));
-let success = document.getElementsById('modal_success');
-let show = Array.from(document.getElementsByClassName('show-success'));
+"use strict";
 
-main.classList  = "modal modal_active";
+let modal = document.getElementById("modal_main");
+let success = document.getElementById("modal_success");
+let close = Array.From(document.getElementsByClassName("modal__close"));
+let show = Array.from(document.getElementsByClassName("show-success"));
+
+modal.classList = "modal modal_active";
+for (let i = 0; i < close.length; i++)
+   close[i].onclick = () => {
+      close[i].closest(".modal").className = "modal";
+      if (close[i] === show[0])
+         success.className = "modal modal_active";
+   }
