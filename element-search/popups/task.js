ed2f65495d6ba600c0707350b78cb18b5439966a
Дарья Грачева <daherishka@yandex.ru>
2022-09-08 22:15:03 +0500

    1st is not done

--- element-search/popups/task.js
+++ element-search/popups/task.js
@@ -5,10 +5,4 @@ let success = document.getElementById("modal_success");
 let close = Array.From(document.getElementsByClassName("modal__close"));
 let show = Array.from(document.getElementsByClassName("show-success"));
 
-modal.classList = "modal modal_active";
-for (let i = 0; i < close.length; i++)
-   close[i].onclick = () => {
-      close[i].closest(".modal").className = "modal";
-      if (close[i] === show[0])
-         success.className = "modal modal_active";
-   }
+modal.className = "modal modal_active";
