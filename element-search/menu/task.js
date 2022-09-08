3eb8672177239d3707a8854ad26a54011023cbbd
Дарья Грачева <daherishka@yandex.ru>
2022-09-08 16:12:24 +0500

    2nd is worked

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -3,10 +3,10 @@ let subMenu = Array.from(document.querySelectorAll("ul.menu.menu_sub"))
 
 for (let i = 0; i < menu.length; i++){
     menu[i].onclick = function() {
-        const choosen = menu[i].closest(".menu__item").querySelector("menu_sub");
+        const choosen = menu[i].closest(".menu__item").querySelector(".menu_sub");
         const show = subMenu.find((item) => item.classList.contains("menu_active"));
 
-        if (choosen){
+        if (choosen) {
             if (show === undefined){
                 choosen.classList.add('menu_active');
             } else {
@@ -19,3 +19,6 @@ for (let i = 0; i < menu.length; i++){
         }
     };
 }
+
+
+
