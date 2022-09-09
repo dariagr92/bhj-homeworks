337e5e1bd7cdbca41abe157ade013e3fdbb49151
Дарья Грачева <daherishka@yandex.ru>
2022-09-09 11:52:59 +0500

    1st finished

--- element-search/popups/task.js
+++ element-search/popups/task.js
@@ -1,8 +1,22 @@
 "use strict";
 
-let modal = document.getElementById("modal_main");
-let success = document.getElementById("modal_success");
-let close = Array.From(document.getElementsByClassName("modal__close"));
-let show = Array.from(document.getElementsByClassName("show-success"));
+let modal = document.querySelector("#modal_main");
+let success = document.querySelector("#modal_success");
+let close = document.querySelectorAll("div.modal__close");
+let show = document.querySelector(".show-success");
 
 modal.className = "modal modal_active";
+
+    show.onclick = function () {
+        modal.className = "modal";
+        success.className = "modal modal_active";
+    }
+
+    for (let i = 0; i < close.length; i++) {
+        close[i].onclick = function () {
+        let activeClose = close[i];
+          if (activeClose.closest(".modal_active")) {
+            activeClose.closest(".modal_active").className = "modal";
+          }
+        }
+      }
