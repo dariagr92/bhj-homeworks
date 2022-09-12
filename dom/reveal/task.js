d5103aeb7b95f59a5efbc2d1b37dc9d072df34d8
Дарья Грачева <daherishka@yandex.ru>
2022-09-12 12:24:00 +0500

    1st is done

---
+++ dom/reveal/task.js
@@ -0,0 +1,12 @@
+const reveal = document.querySelectorAll('.reveal');
+
+for (let i = 0; i < reveal.length; i++){
+    window.addEventListener('scroll', () => {
+        const { top, bottom } = reveal[i].getBoundingClientRect();
+        if ((bottom <  0) || (top > window.innerHeight)){
+            reveal[i].classList.remove('reveal_active');
+        } else {
+            reveal[i].classList.add('reveal_active');
+        }
+    })
+}
