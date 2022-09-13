639e56c38f032f270010d266a7995326b7a13d6f
Дарья Грачева <daherishka@yandex.ru>
2022-09-13 17:34:40 +0500

    3rd corrected

--- dom/book-reader/task.js
+++ dom/book-reader/task.js
@@ -4,8 +4,8 @@ const book = document.querySelector('.book');
 for (let i = 0; i < buttonSize.length; i++){
     buttonSize[i].addEventListener('click', (e) => {
         const activeSize = document.querySelector('.font-size_active');
-        activeSize.classList.remove('.font-size_active');
-        buttonSize[i].classList.add('.font-size_active');
+        activeSize.classList.remove('font-size_active');
+        buttonSize[i].classList.add('font-size_active');
 
         if (buttonSize[i].dataset.size === "small"){
             book.classList.remove('book_fs-big');
