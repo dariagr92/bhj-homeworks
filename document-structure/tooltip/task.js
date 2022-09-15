3ea6da38f145aa71094c0e4c0605ccab9268e3c6
Дарья Грачева <daherishka@yandex.ru>
2022-09-15 14:40:30 +0500

    1st corrected

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -1,17 +1,31 @@
 const tooltips = Array.from(document.querySelectorAll('.has-tooltip'));
 
 tooltips.forEach(tooltip => {
-    const hint = document.createElement('div');
-    hint.textContent = tooltip.title;
 
+        
+    
     tooltip.addEventListener('click', (event) => {
         event.preventDefault();
+        const activeTooltip = Array.from(document.querySelectorAll('.tooltip_active'));
+
+        let hint = document.createElement('div');
+        hint.textContent = tooltip.title;
         hint.classList.add('tooltip');
-        hint.classList.toggle('tooltip_active');
-        tooltip.insertAdjacentElement("beforeBegin", hint);
+        hint.classList.add('tooltip_active');
+        tooltip.insertAdjacentElement("afterBegin", hint);
+        
+
         hint.style.position = "absolute";
         let position = tooltip.getBoundingClientRect();
         hint.style.left = `${position.left}px`;
         hint.style.top = `${position.bottom}px`;
-     });
+
+        activeTooltip.forEach(item => {
+            if (item.classList.contains('tooltip_active')){
+                item.classList.remove('tooltip_active');
+            } else {
+                item.classList.add('tooltip_active');
+            }
+     })
+});
 });
