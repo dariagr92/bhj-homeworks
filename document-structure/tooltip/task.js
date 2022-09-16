a1769a5411e3c13fabcb7474adaf0a937728acd7
Дарья Грачева <daherishka@yandex.ru>
2022-09-16 20:45:19 +0500

    tooptip corrected

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -1,7 +1,4 @@
-    const hasTooltips = Array.from(document.querySelectorAll('.has-tooltip'));
-
-    
-    
+const hasTooltips = Array.from(document.querySelectorAll('.has-tooltip'));
 
 hasTooltips.forEach((el) => {
     let hint = document.createElement('div');
@@ -9,23 +6,23 @@ hasTooltips.forEach((el) => {
     el.insertAdjacentElement("afterend", hint);
 
     el.addEventListener('click', (e) => {
-    e.preventDefault();
-
-    let tooltipActive = document.querySelector('.tooltip_active');  
+        e.preventDefault();
+        let tooltipActive = document.querySelector('.tooltip_active');  
     
-    if (tooltipActive){
-        if (tooltipActive.textContent === el.title){
-        tooltipActive.classList.toggle('tooltip_active');
+        if (tooltipActive){
+            if (tooltipActive.textContent === el.title){
+            tooltipActive.classList.toggle('tooltip_active');
         } else {  
             tooltipActive.classList.remove('tooltip_active');
             hint.classList.add('tooltip_active');
         }
-    } else {
-        hint.classList.add('tooltip_active');
-    }
+        } else {
+            hint.classList.add('tooltip_active');
+        }
+
         hint.textContent = el.title;
         hint.style.left = `${el.getBoundingClientRect().left}px`;
         hint.style.top = `${el.getBoundingClientRect().bottom + window.scrollY}px`;
         hint.style.position = "absolute";
         });
-       });
+    });
