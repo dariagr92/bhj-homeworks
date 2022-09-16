ced5df53be5a90568e7b3d5d085e487b5558e649
Дарья Грачева <daherishka@yandex.ru>
2022-09-16 20:43:10 +0500

    tooltip is corrected

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -1,24 +1,31 @@
-const hasTooltips = Array.from(document.querySelector('.has-tooltip'));
-let tooltip = document.querySelector('.tooltip');
-let hint = document.createElement('div');   
-hint.classList.add('tooltip');
+    const hasTooltips = Array.from(document.querySelectorAll('.has-tooltip'));
 
-
-hasTooltips.forEach((el) => el.addEventListener('click', (e) => {
-    e.preventDefault();
     
-    hint.style.position = "absolute";
+    
+
+hasTooltips.forEach((el) => {
+    let hint = document.createElement('div');
+    hint.classList.add('tooltip');
     el.insertAdjacentElement("afterend", hint);
 
-    if (hint.textContent === el.title){
-        hint.classList.toggle('tooltip_active');
-    } else {       
-        hint.classList.add('tooltip_active');
-        let position = el.getBoundingClientRect();
-        hint.style.left = `${position.left}px`;
-        hint.style.top = `${position.bottom}px`;
-        hint.textContent = el.title;
-    }
-    
-}));
+    el.addEventListener('click', (e) => {
+    e.preventDefault();
 
+    let tooltipActive = document.querySelector('.tooltip_active');  
+    
+    if (tooltipActive){
+        if (tooltipActive.textContent === el.title){
+        tooltipActive.classList.toggle('tooltip_active');
+        } else {  
+            tooltipActive.classList.remove('tooltip_active');
+            hint.classList.add('tooltip_active');
+        }
+    } else {
+        hint.classList.add('tooltip_active');
+    }
+        hint.textContent = el.title;
+        hint.style.left = `${el.getBoundingClientRect().left}px`;
+        hint.style.top = `${el.getBoundingClientRect().bottom + window.scrollY}px`;
+        hint.style.position = "absolute";
+        });
+       });
